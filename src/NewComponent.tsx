4c5dcb625b704275f9fd5f0326909dedc749b054
m-diana-dev <m.diana.dev@gmail.com>
2023-04-26 15:37:32 +0300

    add 4 microtask

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,3 +1,5 @@
+import './NewComponent.css';
+
 type NewComponentType = {
     students: StudentType[]
 }
@@ -9,13 +11,41 @@ type StudentType = {
 }
 
 export const NewComponent = (props: NewComponentType) => {
-    return(
-        <ul>
-            {props.students.map((el) => {
-                return(
-                    <li key={el.id}>Name {el.name} Age {el.age}</li>
-                )
-            })}
-        </ul>
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
+
+    return (
+        <>
+            <ul>
+                {props.students.map((el) => {
+                    return (
+                        <li key={el.id}>Name {el.name} Age {el.age}</li>
+                    )
+                })}
+            </ul>
+            <h2>Table</h2>
+            <table>
+                <thead>
+                <tr>
+                    <td>manufacturer</td>
+                    <td>model</td>
+                </tr>
+                </thead>
+                <tbody>
+                {topCars.map((el, index) => {
+                    index += 1;
+                    return (
+                        <tr key={index}>
+                            <td>{el.manufacturer}</td>
+                            <td>{el.model}</td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </>
     )
 }
